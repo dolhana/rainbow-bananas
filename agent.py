27ee4f8b14a77237a03b96ad2135f2229f4956f0
Handol Park <handolpark12@gmail.com>
2018-11-17 13:32:10 -0800

    DQNAgent passes basic tests

--- agent.py
+++ agent.py
@@ -1,35 +1,126 @@
 import random
+import copy
 from collections import namedtuple, deque
+
+import numpy as np
 
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
+import model
 
-class Agent():
-    """Interacts with and learns from the environment."""
 
-    def __init__(self, state_size, action_size, seed):
+class DQNAgent():
+    """DQN agent is an agent that uses DQN as a value network.
+    """
+
+    def __init__(self,
+                 state_size, n_actions,
+                 hidden_units=[64, 64],
+                 learning_rate=5e-4,
+                 batch_size=64,
+                 gamma=0.99,
+                 replay_buffer_capacity=10**5,
+                 update_interval=4,
+                 tau=1e-3,
+                 device="cpu"):
         """Initialize the agent
 
         Args:
-          state_size (int): dimension of each state
-          action_size (int): dimension of each action
+          state_size (int): the dimension of a state
+          n_actions (int): the number of actions that the agent can choose from
           seed (int): random seed
         """
         self.state_size = state_size
-        self.action_size = action_size
-        self.seed = seed
-        random.seed(seed)
+        self.n_actions = n_actions
+        self.hidden_units = hidden_units
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.gamma = gamma
+        self.replay_buffer_capacity = replay_buffer_capacity
+        self.update_interval = update_interval
+        self.device = device
 
-    def step(self):
-        """The agent takes one step in the environment"""
+        self.online_network = model.QNetwork(state_size, n_actions, hidden_units).to(device)
+        self.target_network = copy.deepcopy(self.online_network)
+        self.optimizer = optim.Adam(self.online_network.parameters(), learning_rate)
+        self.losser = torch.nn.MSELoss()
 
-    def choose_action(self, state, epsilon=0.):
-        """The agent chooses an action based on the given state"""
+        # TODO Does replay_buffer need more ecapsulation?
+        self.replay_buffer = []
 
-    def learn(self, experiences, gamma):
-        """The agent learns from the experience"""
+        self.step_count = 0
 
-    def soft_update(self, tau):
-        """Updates the target network so that it moves toward to the local network"""
+    def choose_action(self, state, training=True):
+        """The agent chooses an action based on the given state
+
+        Returns:
+          int: the action index that maximize the return at a given state
+        """
+        if random.random() >= self._epsilon():
+            return np.argmax(self.action_values(state))
+        else:
+            return random.randint(0, self.n_actions - 1)
+
+    def _epsilon(self):
+        return max(1. - .9 * self.step_count / 1000., 0.1)
+
+    def action_values(self, state):
+        """Calculates vales of all actions at a given state
+
+        Returns:
+          numpy.array(): the values of all actions
+        """
+        return self.online_network(state).cpu().detach().numpy()
+
+    def observe_and_learn(self, state, action, reward, next_state, done):
+        """Observes and stores an experience
+        """
+        # step_count is needed for soft-update, (TODO) epsilon shaping and (TODO) learning_rate shaping
+        step_count += 1
+
+        # TODO Is limiting the size of replay_buffer by slicing efficient enough?
+        self.replay_buffer.append((state, action, reward, next_state, done))
+        self.replay_buffer = self.replay_buffer[-self.replay_buffer_capacity:]
+
+        # TODO Sample a batch from the replay_buffer
+        experiece_sample = random.choices(self.replay_buffer, k=self.batch_size)
+
+        self.learn(experience_sample)
+
+        if (step_count % self.update_interval == 0
+            and len(self.replay_buffer) >= self.batch_size):
+            self.soft_update()
+
+    def learn(self, experiences):
+        """Learns from experiences
+        """
+        s0, r, a, s1, done = (np.vstack(e) for e in zip(*experiences))
+
+        # TD target
+        s1_value = self.target_network(s1).detach().max(dim=1)
+        td_target = r + np.logical_not(done) * gamma * s1_value
+        expteced = self.online_network(s0).gather(dim=1, index=a)
+
+        # Loss
+        loss = self.losser.loss(expected, td_target)
+
+        # Minimize the loss
+        self.optimizer.zero_grad()
+        loss.backword()
+        self.optimizer.step()
+
+    def soft_update(self):
+        """Updates the target network so that it moves toward to the online network
+        """
+        for target_param, online_param in \
+            zip(self.target_network.parameters(), self.online_network.parameters()):
+            target_param.data.copy_(self.tau)
+
+
+def manual_random_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.manual_seed(seed)
